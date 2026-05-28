385e793b85dac6d9389cf473167e9c3033e59c6f
iamdipanshugupta <kumardipanshu983542@gmail.com>
2026-05-28 08:31:02 +0530

    Add About, Contact, and Auth pages with basic structure

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Link from 'next/link'
 const AboutPage = () => {
   return (
     <div>
+      <Link href="/contact">Go to home</Link>
       About Page
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <h1>Header</h1>
         {children}
+        <h1>Footer</h1>
         </body>
     </html>
   );
